fcf99aa729e2f3f3ebf1e96c9287594e95602719
Tatkeu Mbiakop <maveltatkeu@gmail.com>
2025-09-07 12:00:24 +0400

    finalizing tests

---
+++ Junit5-Tdd/src/test/java/com/openclassrooms/testing/calcul/acceptance/CucumberAIT.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.testing.calcul.acceptance;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = "src/test/resources/features", plugin = { "pretty", "html:target/html-cucumber-report" })
+public class CucumberAIT {
+//  @Given("un élève utilise le Calculateur")
+//  public void a_student_is_using_the_Calculator() {
+//
+//  }
+//
+//  @When("{int} et {int} sont additionnés")
+//  public void and_are_added(Integer leftArgument, Integer rightArgument) {
+//
+//  }
+//
+//  @Then("on montre {int} à l'élève")
+//  public void the_student_is_shown(Integer expectedResult) {
+//
+//  }
+}
